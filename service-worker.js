2e7762659a61066f18b3b73a0ed45087e5905470
Lucas Araújo <lucasfelipemk@gmail.com>
2025-11-12 20:10:16 -0300

    🔧 chore: setup of basic `service-worker` for can running PWA offline

---
+++ service-worker.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'pwa-cache-v1';
+const URLS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/product1.html',
+  '/product2.html',
+  '/product3.html',
+  '/product4.html',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+});
